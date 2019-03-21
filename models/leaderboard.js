f63353f2873d3a7169e21edcc6b66b1e5234bcbc
Marc D'Mello <marcd2000@gmail.com>
2019-03-20 22:47:46 -0400

    Changed leaderboard.js

--- models/leaderboard.js
+++ models/leaderboard.js
@@ -10,7 +10,7 @@ admin.initializeApp({
     credential: admin.credential.cert({
         projectId: process.env.PROJECT_ID,
         clientEmail: process.env.CLIENT_EMAIL,
-        privateKey: process.env.PRIVATE_KEY
+        privateKey: process.env.PRIVATE_KEY,
     }),
     databaseURL: 'https://aperacer-39c23.firebaseio.com'
 });
